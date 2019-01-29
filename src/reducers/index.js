71f9c5263a81edb97ce31085e97b49bac87e760a
Jamie Pham <jtkp66@gmail.com>
2019-01-29 13:39:32 -0800

    Initial commit. Created file structure, and added action creators and reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        { title: 'No scrubs', duration: '4:05'},
+        { title: 'macarena', duration: '2:30'},
+        { title: 'No scrubs', duration: '3:15'},
+        { title: 'i want it', duration: '1:45'}
+    ];
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
+
+// export default now any other file in the project can get accesss to the combined reducers
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
